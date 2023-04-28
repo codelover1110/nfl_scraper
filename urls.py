8fedb3bc58e705af4daef1160880fffdff425755
Script Kiddie <44860661+jotdevelopers@users.noreply.github.com>
2023-04-29 01:58:59 +0500

    URLs Updated.

--- urls.py
+++ urls.py
@@ -91,6 +91,7 @@ def main():
     with engine.connect() as conn:
         records = conn.execute(my_table.select()).fetchall()
         for record in records:
+            print(record.url)
             gs.navigate_to_url(record.url)
             time.sleep(15)
             # close cookies
@@ -107,11 +108,27 @@ def main():
                 # iterate through quarters
                 for match in match_elements:
                     nfl_url_data = {}
+                    g_dt_final = None
+
                     soup = BeautifulSoup(match.get_attribute('innerHTML'), 'html.parser')
                     infos = soup.find_all('div',
                                           class_='css-text-1rynq56 r-color-1khnkhu r-fontFamily-1fdbu1n r-fontSize-ubezar')
-                    print("infos", infos)
+
+                    game_date = soup.find('div', class_ = 'css-text-1rynq56 r-color-1khnkhu r-fontFamily-1fdbu1n r-fontSize-1enofrn r-lineHeight-1cwl3u0 r-paddingLeft-m2pi6t r-textAlign-q4m81j')
+                    g_dt = str(game_date)
+                    # print(g_dt)
+                    # game_date = game_date[1]
+                    if game_date:
+                        game_date = g_dt.split(">")
+                        game_d = game_date[1].split("<")
+                        game_d = game_d[0].split(' ')
+                        game_day = game_d[0]
+                        game_month = game_d[1].split('/')
+                        g_dt_final = str(record.nfl_year)+"-"+str(game_month[0])+"-"+str(game_month[1])
+                        # print("Game Date ======================> ", g_dt_final)
+
                     if infos:
+                        # print("infos", infos)
                         gameid = gameid + 1
                         home = infos[0].text
                         away = infos[1].text
@@ -137,17 +154,154 @@ def main():
                         nfl_url_data['away'] = away
                         nfl_url_data['yr'] = record.nfl_year
                         nfl_url_data['week_no'] = record.week_no
+                        nfl_url_data['date'] = g_dt_final
                         nfl_url_data['week_name'] = record.week_name
                         print(nfl_url_data)
                         #game_data_detail.append(nfl_url_data)
 
                         data = pd.json_normalize(nfl_url_data)
-
-                        table_name = 'nfl_game_urls'
+                        # print(data)
+                        table_name = 'nfl_game_urls-v1'
                         data.to_sql(table_name, engine, if_exists='append', index=False)
-            except:
+            except Exception as e:
+                print(e)
                 pass
 
+
+
+    # for yr in range(2017, year):
+    #     if yr <= 2020:
+    #         for week in range(1, 18):
+    #             url = 'https://www.nfl.com/scores/' + str(yr) + '/REG' + str(week)
+    #             print(url)
+    #             gs.navigate_to_url(url)
+    #             time.sleep(20)
+    #             # close cookies
+    #             try:
+    #                 gs.select_element(By.XPATH, '//*[@id="onetrust-close-btn-container"]/button').click()
+    #             except:
+    #                 pass
+    #
+    #             # First, we need to get all the data from all available quarters
+    #             gs.wait_for_element(By.XPATH, './/div[@class="css-37urdo"]')
+    #             match_elements = gs.select_element(By.XPATH, './/div[@class="css-37urdo"]').find_elements(By.XPATH,
+    #                                                                                                       './/div[@class="css-156uxf7"]')
+    #             try:
+    #                 # iterate through quarters
+    #                 for match in match_elements:
+    #                     nfl_url_data = {}
+    #                     soup = BeautifulSoup(match.get_attribute('innerHTML'), 'html.parser')
+    #                     infos = soup.find_all('div',
+    #                                           class_='css-text-1rynq56 r-color-1khnkhu r-fontFamily-1fdbu1n r-fontSize-ubezar')
+    #                     print("infos", infos)
+    #                     if infos:
+    #                         gameid = gameid + 1
+    #                         home = infos[0].text
+    #                         away = infos[1].text
+    #
+    #                         nfl_url_data['gameid'] = gameid
+    #                         nfl_url_data['url'] = "https://www.nfl.com/games/" + str(home.lower()) + "-at-" + str(
+    #                             away.lower()) + "-" + str(yr) + "-reg-" + str(week)
+    #                         nfl_url_data['home'] = home
+    #                         nfl_url_data['away'] = away
+    #                         nfl_url_data['year'] = yr
+    #                         nfl_url_data['week'] = week
+    #                         print(nfl_url_data)
+    #                         game_data_detail.append(nfl_url_data)
+    #             except:
+    #                 pass
+    #     else:
+    #         for week in range(1, 19):
+    #             url = 'https://www.nfl.com/scores/' + str(yr) + '/REG' + str(week)
+    #             print(url)
+    #             gs.navigate_to_url(url)
+    #             time.sleep(20)
+    #             # close cookies
+    #             try:
+    #                 gs.select_element(By.XPATH, '//*[@id="onetrust-close-btn-container"]/button').click()
+    #             except:
+    #                 pass
+    #
+    #             # First, we need to get all the data from all available quarters
+    #             gs.wait_for_element(By.XPATH, './/div[@class="css-37urdo"]')
+    #             match_elements = gs.select_element(By.XPATH, './/div[@class="css-37urdo"]').find_elements(By.XPATH,
+    #                                                                                                       './/div[@class="css-156uxf7"]')
+    #             try:
+    #                 # iterate through quarters
+    #                 for match in match_elements:
+    #                     nfl_url_data = {}
+    #                     soup = BeautifulSoup(match.get_attribute('innerHTML'), 'html.parser')
+    #                     infos = soup.find_all('div',
+    #                                           class_='css-text-1rynq56 r-color-1khnkhu r-fontFamily-1fdbu1n r-fontSize-ubezar')
+    #                     print("infos", infos)
+    #                     if infos:
+    #                         gameid = gameid + 1
+    #                         home = infos[0].text
+    #                         away = infos[1].text
+    #
+    #                         nfl_url_data['gameid'] = gameid
+    #                         nfl_url_data['url'] = "https://www.nfl.com/games/" + str(home.lower()) + "-at-" + str(
+    #                             away.lower()) + "-" + str(yr) + "-reg-" + str(week)
+    #                         nfl_url_data['home'] = home
+    #                         nfl_url_data['away'] = away
+    #                         nfl_url_data['year'] = yr
+    #                         nfl_url_data['week'] = week
+    #                         print(nfl_url_data)
+    #                         game_data_detail.append(nfl_url_data)
+    #             except:
+    #                 pass
+    #
+    # for yr in range(2017, year):
+    #     for week in range(1, 5):
+    #         url = 'https://www.nfl.com/scores/' + str(yr) + '/POST' + str(week)
+    #
+    #         if week == 1:
+    #             weeek = 19
+    #         elif week == 2:
+    #             weeek = 20
+    #         elif week == 3:
+    #             weeek = 21
+    #         elif week == 4:
+    #             weeek = 22
+    #
+    #
+    #         print(url)
+    #         gs.navigate_to_url(url)
+    #         time.sleep(20)
+    #         # close cookies
+    #         try:
+    #             gs.select_element(By.XPATH, '//*[@id="onetrust-close-btn-container"]/button').click()
+    #         except:
+    #             pass
+    #
+    #         # First, we need to get all the data from all available quarters
+    #         gs.wait_for_element(By.XPATH, './/div[@class="css-37urdo"]')
+    #         match_elements = gs.select_element(By.XPATH, './/div[@class="css-37urdo"]').find_elements(By.XPATH,
+    #                                                                                                   './/div[@class="css-156uxf7"]')
+    #         try:
+    #             # iterate through quarters
+    #             for match in match_elements:
+    #                 nfl_url_data = {}
+    #                 soup = BeautifulSoup(match.get_attribute('innerHTML'), 'html.parser')
+    #                 infos = soup.find_all('div',
+    #                                       class_='css-text-1rynq56 r-color-1khnkhu r-fontFamily-1fdbu1n r-fontSize-ubezar')
+    #                 print("infos", infos)
+    #                 if infos:
+    #                     gameid = gameid + 1
+    #                     home = infos[0].text
+    #                     away = infos[1].text
+    #
+    #                     nfl_url_data['gameid'] = gameid
+    #                     nfl_url_data['url'] = "https://www.nfl.com/games/" + str(home.lower()) + "-at-" + str(
+    #                         away.lower()) + "-" + str(yr) + "-post-" + str(week)
+    #                     nfl_url_data['home'] = home
+    #                     nfl_url_data['away'] = away
+    #                     nfl_url_data['year'] = yr
+    #                     nfl_url_data['week'] = weeek
+    #                     print(nfl_url_data)
+    #                     game_data_detail.append(nfl_url_data)
+    #         except:
+    #             pass
 
 
 def work():
